7b271514bff47d9ad4beed2a10132ef3edc10297
Rustam Gabdelislamov <rustamgabdelislamov911@gmail.com>
2025-05-14 00:14:52 +0500

    ok

--- decorators/log.py
+++ decorators/log.py
@@ -7,11 +7,12 @@ def log(filename=""):
             time_1 = time.time()
             print(f'Function {func.__name__} started at {time_1}')
             result = func(*args, **kwargs)
-            if "*" in result :
+            if "*" in result:
                 log_message = f"{func.__name__} ok"
                 if filename:
                     with open(filename, 'a', encoding='utf-8') as file:
                         file.write(log_message + '\n')
+                    print(log_message)
                 else:
                     print(log_message)
                 return result
@@ -22,6 +23,7 @@ def log(filename=""):
                 if filename:
                     with open(filename, 'a', encoding='utf-8') as file:
                         file.write(error_message + '\n')
+                    print(error_message)
                 else:
                     print(error_message)
                 return result
--- tests/test_log.py
+++ tests/test_log.py
@@ -1,5 +1,3 @@
-import pytest
-
 from decorators.log import log
 from src.masks import get_mask_card_number
 
@@ -12,9 +10,34 @@ def get_mask_card_number(card_number: str) -> str:
         return f'{card_number[0:4]} {card_number[4:6]}** **** {card_number[12:]}'
     return 'Неправильный номер карты'
 
+
 def test_ok_number():
     assert get_mask_card_number("1234123412341234") == "1234 12** **** 1234"
 
 
 def test_error():
     assert get_mask_card_number("12345") == "Неправильный номер карты"
+
+
+def test_ok_number_(capsys):
+    # Вызов функции с корректным номером карты
+    result = get_mask_card_number("1234123412341234")
+
+    # Проверка результата
+    assert result == "1234 12** **** 1234"
+
+    # Проверка вывода в лог
+    captured = capsys.readouterr()
+    assert "get_mask_card_number ok" in captured.out
+
+
+def test_error_(capsys):
+    # Вызов функции с некорректным номером карты
+    result = get_mask_card_number("12345")
+
+    # Проверка результата
+    assert result == "Неправильный номер карты"
+
+    # Проверка вывода в лог
+    captured = capsys.readouterr()
+    assert "get_mask_card_number error: Неправильный номер карты. Inputs: 12345" in captured.out
